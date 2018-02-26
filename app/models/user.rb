052e94c41fabaa810c55490105b78ac095c1b61f
akimuroi <neoworld7521@yahoo.co.jp>
2018-02-26 13:51:21 +0000

    facebook_login

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+
+    user
+  end
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+end
